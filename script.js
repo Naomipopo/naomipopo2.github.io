88c885996f7db7488b23eeefb5219739b61efc9a
Naomipopo <105216291+Naomipopo@users.noreply.github.com>
2022-06-23 22:53:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 alert("Introduction to JavaScript");
+document.write("<h1>Name:Popo-Olaniyan Oluwatofunmi</h2>");
+document.write("<h1>Country:Nigeria</h1>");
+document.write("<h1>Height:1.63metres</h1>");
+
 let Name = "Popo-Olaniyan Oluwatofunmi Naomi";
 let Height = "1.63metres";
 let Country = "Nigeria";
